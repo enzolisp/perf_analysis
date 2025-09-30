a7d55f5caec08b8ecfa702f3b65f74c9dca68973
enzolisp <enzolisp@gmail.com>
2025-09-30 16:23:13 -0300

    script.sh docker group check

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# sudo groupadd dockerd
-# sudo usermod -aG docker ${USER}
-# newgrp docker
+if [[ ! "$(groups)" == *"docker"* ]]; then
+    sudo groupadd dockerd
+    sudo usermod -aG docker ${USER}
+    newgrp docker
+fi
 
-docker pull jupyter/scipy-notebook
-docker run --rm -p 8888:8888 --name jupyter -v "${PWD}/src":/home/jovyan/work jupyter/scipy-notebook
+#docker pull jupyter/scipy-notebook
+#docker run --rm -p 8888:8888 --name jupyter -v "${PWD}/src":/home/jovyan/work jupyter/scipy-notebook
 #docker stats jupyter
 
