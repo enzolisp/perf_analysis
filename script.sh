50e75c002b285812307dd8c0393489d64cab298f
enzolisp <enzolisp@gmail.com>
2025-09-30 20:09:19 -0300

    script.sh update so it strea ms files into docker_metrics.csv

--- script.sh
+++ script.sh
@@ -6,7 +6,8 @@ if [[ ! "$(groups)" == *"docker"* ]]; then
     newgrp docker
 fi
 
-#docker pull jupyter/scipy-notebook
-#docker run --rm -p 8888:8888 --name jupyter -v "${PWD}/src":/home/jovyan/work jupyter/scipy-notebook
-#docker stats jupyter
+docker pull jupyter/scipy-notebook
+docker run -d --rm -p 8888:8888 --name jupyter -v "${PWD}/src":/home/jovyan/work jupyter/scipy-notebook
+(echo "CPU_PERC,MEM_USAGE,NET_IO,BLOCK_IO"; docker stats --no-stream --format "{{.CPUPerc}},{{.MemUsage}},{{.NetIO}},{{.BlockIO}}") > docker_metrics.csv
+#sed 's/\[H|\[K|\[J//g' -nrw docker_metrics.csv 
 
