2b06712d72abe9ff0d28b5908457b73ef1e301a5
enzolisp <enzolisp@gmail.com>
2025-10-01 20:56:39 -0300

    script.sh change in csv_name

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # sampling rate (seconds)
-SAMPLING=0.5
-
+SAMPLING=1
+csv_name=`date +"%d_%m_%Y-%H:%M:%S.csv"`
 
 if [[ ! "$(groups)" == *"docker"* ]]; then
     sudo groupadd docker
@@ -12,7 +12,7 @@ fi
 
 docker pull jupyter/scipy-notebook
 docker run -d --rm -p 8888:8888 --name jupyter -v "${PWD}/src":/home/jovyan/work jupyter/scipy-notebook
-while true; do docker stats --no-stream | cat >> ./experiments-csv/`date +"%d_%m_%Y - %H:%M:%S.csv"`; sleep $SAMPLING; done
+while true; do docker stats --no-stream >> ./experiments-csv/$csv_name; sleep $SAMPLING; done
 #(echo "CPU_PERC,MEM_USAGE,NET_IO,BLOCK_IO"; watch -n 1 docker stats --no-stream --format "{{.CPUPerc}},{{.MemUsage}},{{.NetIO}},{{.BlockIO}}\n") >> docker_metrics/docker_metrics.csv
 #sed 's/\[H|\[K|\[J//g' -nrw docker_metrics.csv 
 docker stop jupyter
