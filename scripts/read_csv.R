65323b70c0938815debd99cf96197e62cd0f02a7
enzolisp <enzolisp@gmail.com>
2025-11-29 14:10:43 -0300

    read_csv_performance function

--- scripts/read_csv.R
+++ scripts/read_csv.R
@@ -29,3 +29,29 @@ read_csv_results <- function() {
     df_list <- bind_rows(df_list)
     return(df_list)
 }
+
+read_csv_performance <- function() {
+  
+    files <- list.files(
+        path = "stats/performance/", 
+        pattern = "_performance\\.csv$",
+        full.names = TRUE 
+    )
+    
+    if (length(files) == 0) {
+        stop("ERROR: 'performance.csv' file found.")
+    }
+    
+    print(paste(length(files), "CSV files found."))
+    
+    df_list <- lapply(files, function(arq) {
+        df <- read_csv(arq, show_col_types = FALSE)
+        #names(df)[names(df) == "L_value"] <- "L_Value"
+        #names(df)[names(df) == "avg_time"] <- "t_exec"
+        
+        return(df)
+    })
+ 
+    df_list <- bind_rows(df_list)
+    return(df_list)
+}
