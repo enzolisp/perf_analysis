18a158a557bcd2ad71e7e278f8171147c0f69a5d
KarablasteNM <mattes734@gmail.com>
2025-11-30 09:37:02 -0300

    Preparação etapa 3, criação do relatório e apresentação

--- src/julia/1d.jl
+++ src/julia/1d.jl
@@ -1,4 +1,4 @@
-# Stage 2 - Comp. Sys. Perf Analysis (2025/2) - Lucas M. Schnorr
+# Stage 3 - Comp. Sys. Perf Analysis (2025/2) - Lucas M. Schnorr
 # Group F: Enzo Lisboa Peixoto - 00584827, Nathan Mattes - 00342941 e Pedro Scholz Soares - 00578266
 
 function run_simulation(L)
